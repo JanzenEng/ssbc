6eae9059dbb1eb2a438c58342b83f4184f6982bb
JanzenEng <35594142+JanzenEng@users.noreply.github.com>
2025-08-01 18:55:33 +0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,23 @@ function switchTab(page) {
     // Redirect to the specified page
     window.location.href = page;
 }
+
+window.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.getElementById("toggle-sidebar");
+    const sidebar = document.querySelector(".sidebar");
+
+    // Toggle sidebar on button click
+    toggleBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("show");
+    });
+
+    // Close sidebar if click outside
+    document.addEventListener("click", (event) => {
+        const isClickInsideSidebar = sidebar.contains(event.target);
+        const isClickOnToggle = toggleBtn.contains(event.target);
+
+        if (!isClickInsideSidebar && !isClickOnToggle) {
+            sidebar.classList.remove("show");
+        }
+    });
+});
